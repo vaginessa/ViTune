674cd1951609a0a2487d7950d54e9b26a0740a75
25huizengek1 <50515369+25huizengek1@users.noreply.github.com>
2023-11-16 15:10:43 +0100

    Bump version to 0.5.7 and add fastlane changelogs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "it.vfsfitvnm.vimusic"
         minSdk = 21
         targetSdk = 34
-        versionCode = 22
-        versionName = "0.5.6"
+        versionCode = 23
+        versionName = "0.5.7"
         multiDexEnabled = true
     }
 
