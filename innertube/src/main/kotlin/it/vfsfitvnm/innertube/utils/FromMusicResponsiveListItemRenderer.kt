4aae4c717588ff76bea4d0d1e03690e41edf4f6b
25huizengek1 <50515369+25huizengek1@users.noreply.github.com>
2023-12-07 02:14:09 +0100

    Fix infinite loading with some mood playlists ('podcasts' probably not supported by privacy-friendly whatever.social API) (closes #28)

--- innertube/src/main/kotlin/it/vfsfitvnm/innertube/utils/FromMusicResponsiveListItemRenderer.kt
+++ innertube/src/main/kotlin/it/vfsfitvnm/innertube/utils/FromMusicResponsiveListItemRenderer.kt
@@ -3,7 +3,6 @@ package it.vfsfitvnm.innertube.utils
 import it.vfsfitvnm.innertube.Innertube
 import it.vfsfitvnm.innertube.models.MusicResponsiveListItemRenderer
 import it.vfsfitvnm.innertube.models.NavigationEndpoint
-import it.vfsfitvnm.innertube.models.Runs
 
 fun Innertube.SongItem.Companion.from(renderer: MusicResponsiveListItemRenderer) =
     Innertube.SongItem(
@@ -14,14 +13,20 @@ fun Innertube.SongItem.Companion.from(renderer: MusicResponsiveListItemRenderer)
             ?.text
             ?.runs
             ?.getOrNull(0)
-            ?.let(Innertube::Info),
+            ?.let {
+                if (it.navigationEndpoint?.endpoint is NavigationEndpoint.Endpoint.Watch) Innertube.Info(
+                    name = it.text,
+                    endpoint = it.navigationEndpoint.endpoint as NavigationEndpoint.Endpoint.Watch
+                ) else null
+            },
         authors = renderer
             .flexColumns
             .getOrNull(1)
             ?.musicResponsiveListItemFlexColumnRenderer
             ?.text
             ?.runs
-            ?.map<Runs.Run, Innertube.Info<NavigationEndpoint.Endpoint.Browse>>(Innertube::Info)
+            ?.map { Innertube.Info(name = it.text, endpoint = it.navigationEndpoint?.endpoint) }
+            ?.filterIsInstance<Innertube.Info<NavigationEndpoint.Endpoint.Browse>>()
             ?.takeIf(List<Any>::isNotEmpty),
         durationText = renderer
             .fixedColumns
