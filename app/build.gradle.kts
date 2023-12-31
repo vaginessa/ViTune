223cd3503229e13cc522c14dc0d1471f96b96125
25huizengek1 <50515369+25huizengek1@users.noreply.github.com>
2023-12-31 13:41:46 +0100

    Bump to v0.6.2 and add fastlane changelogs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 21
         targetSdk = 34
 
-        versionCode = System.getenv("ANDROID_VERSION_CODE")?.toIntOrNull() ?: 29
+        versionCode = System.getenv("ANDROID_VERSION_CODE")?.toIntOrNull() ?: 30
         versionName = project.version.toString()
 
         multiDexEnabled = true
