238f11b45474c126b383c66470eaafb6a4704aa0
25huizengek1 <50515369+25huizengek1@users.noreply.github.com>
2024-01-15 11:43:12 +0100

    Filter on actual local music (avoid ghost files in MediaStore) (closes #72)

--- app/src/main/kotlin/it/vfsfitvnm/vimusic/ui/screens/home/HomeLocalSongs.kt
+++ app/src/main/kotlin/it/vfsfitvnm/vimusic/ui/screens/home/HomeLocalSongs.kt
@@ -49,6 +49,7 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.isActive
@@ -83,7 +84,7 @@ fun HomeLocalSongs(onSearchClick: () -> Unit) = with(OrderPreferences) {
                 sortBy = localSongSortBy,
                 sortOrder = localSongSortOrder,
                 isLocal = true
-            )
+            ).map { songs -> songs.filter { it.durationText != "0:00" } }
         },
         sortBy = localSongSortBy,
         setSortBy = { localSongSortBy = it },
@@ -129,6 +130,7 @@ fun Context.musicFilesAsFlow(): StateFlow<List<Song>> = flow {
                 if (isAtLeastAndroid10) MediaStore.Audio.Media.getContentUri(MediaStore.VOLUME_EXTERNAL)
                 else MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
             val projection = arrayOf(
+                MediaStore.Audio.Media.IS_MUSIC,
                 MediaStore.Audio.Media._ID,
                 MediaStore.Audio.Media.DISPLAY_NAME,
                 MediaStore.Audio.Media.DURATION,
@@ -140,6 +142,7 @@ fun Context.musicFilesAsFlow(): StateFlow<List<Song>> = flow {
 
             contentResolver.query(collection, projection, null, null, sortOrder)
                 ?.use { cursor ->
+                    val isMusicIdx = cursor[MediaStore.Audio.Media.IS_MUSIC]
                     val idIdx = cursor[MediaStore.Audio.Media._ID]
                     val nameIdx = cursor[MediaStore.Audio.Media.DISPLAY_NAME]
                     val durationIdx = cursor[MediaStore.Audio.Media.DURATION]
@@ -148,9 +151,11 @@ fun Context.musicFilesAsFlow(): StateFlow<List<Song>> = flow {
 
                     buildList {
                         while (cursor.moveToNext()) {
+                            if (cursor.getInt(isMusicIdx) == 0) continue
                             val id = cursor.getLong(idIdx)
                             val name = cursor.getString(nameIdx)
                             val duration = cursor.getInt(durationIdx)
+                            if (duration == 0) continue
                             val artist = cursor.getString(artistIdx)
                             val albumId = cursor.getLong(albumIdIdx)
 
@@ -159,6 +164,7 @@ fun Context.musicFilesAsFlow(): StateFlow<List<Song>> = flow {
                                 duration.milliseconds.toComponents { minutes, seconds, _ ->
                                     "$minutes:${seconds.toString().padStart(2, '0')}"
                                 }
+
                             add(
                                 Song(
                                     id = "$LOCAL_KEY_PREFIX$id",
