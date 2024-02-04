b6e1e4bc21e85174790a5b5882c67b4c205a6808
25huizengek1 <50515369+25huizengek1@users.noreply.github.com>
2024-02-04 23:48:44 +0100

    Add swipe gesture/animation to LyricsDialog

--- app/src/main/kotlin/it/vfsfitvnm/vimusic/ui/screens/player/LyricsDialog.kt
+++ app/src/main/kotlin/it/vfsfitvnm/vimusic/ui/screens/player/LyricsDialog.kt
@@ -1,5 +1,11 @@
 package it.vfsfitvnm.vimusic.ui.screens.player
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.ContentTransform
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,8 +22,11 @@ import it.vfsfitvnm.vimusic.Database
 import it.vfsfitvnm.vimusic.LocalPlayerServiceBinder
 import it.vfsfitvnm.vimusic.service.isLocal
 import it.vfsfitvnm.vimusic.ui.modifiers.PinchDirection
+import it.vfsfitvnm.vimusic.ui.modifiers.onSwipe
 import it.vfsfitvnm.vimusic.ui.modifiers.pinchToToggle
 import it.vfsfitvnm.vimusic.ui.styling.LocalAppearance
+import it.vfsfitvnm.vimusic.utils.forceSeekToNext
+import it.vfsfitvnm.vimusic.utils.forceSeekToPrevious
 import it.vfsfitvnm.vimusic.utils.px
 import it.vfsfitvnm.vimusic.utils.thumbnail
 
@@ -39,39 +48,76 @@ fun LyricsDialog(
 
     window ?: return@Dialog
 
-    BoxWithConstraints(
-        modifier = modifier
-            .padding(all = 36.dp)
-            .padding(vertical = 32.dp)
-            .clip(thumbnailShape)
-            .fillMaxSize()
-            .background(colorPalette.background1)
-            .pinchToToggle(
-                direction = PinchDirection.In,
-                threshold = 0.9f,
-                onPinch = { onDismiss() }
+    AnimatedContent(
+        targetState = window,
+        transitionSpec = {
+            if (initialState.mediaItem.mediaId == targetState.mediaItem.mediaId)
+                return@AnimatedContent ContentTransform(
+                    targetContentEnter = EnterTransition.None,
+                    initialContentExit = ExitTransition.None
+                )
+
+            val direction = if (targetState.firstPeriodIndex > initialState.firstPeriodIndex)
+                AnimatedContentTransitionScope.SlideDirection.Left
+            else AnimatedContentTransitionScope.SlideDirection.Right
+
+            ContentTransform(
+                targetContentEnter = slideIntoContainer(
+                    towards = direction,
+                    animationSpec = tween(500)
+                ),
+                initialContentExit = slideOutOfContainer(
+                    towards = direction,
+                    animationSpec = tween(500)
+                ),
+                sizeTransform = null
             )
-    ) {
-        val thumbnailHeight = maxHeight
-
-        if (window.mediaItem.mediaMetadata.artworkUri != null) AsyncImage(
-            model = window.mediaItem.mediaMetadata.artworkUri.thumbnail((thumbnailHeight - 64.dp).px),
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
+        },
+        label = ""
+    ) { currentWindow ->
+        BoxWithConstraints(
+            modifier = modifier
+                .padding(all = 36.dp)
+                .padding(vertical = 32.dp)
+                .clip(thumbnailShape)
                 .fillMaxSize()
-                .background(colorPalette.background0)
-        )
+                .background(colorPalette.background1)
+                .pinchToToggle(
+                    direction = PinchDirection.In,
+                    threshold = 0.9f,
+                    onPinch = { onDismiss() }
+                )
+                .onSwipe(
+                    onSwipeLeft = {
+                        player.forceSeekToNext()
+                    },
+                    onSwipeRight = {
+                        player.seekToDefaultPosition()
+                        player.forceSeekToPrevious()
+                    }
+                )
+        ) {
+            val thumbnailHeight = maxHeight
 
-        Lyrics(
-            mediaId = window.mediaItem.mediaId,
-            isDisplayed = true,
-            onDismiss = { },
-            height = thumbnailHeight,
-            mediaMetadataProvider = window.mediaItem::mediaMetadata,
-            durationProvider = player::getDuration,
-            ensureSongInserted = { Database.insert(window.mediaItem) },
-            onMenuLaunched = onDismiss
-        )
+            if (currentWindow.mediaItem.mediaMetadata.artworkUri != null) AsyncImage(
+                model = currentWindow.mediaItem.mediaMetadata.artworkUri.thumbnail((thumbnailHeight - 64.dp).px),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(colorPalette.background0)
+            )
+
+            Lyrics(
+                mediaId = currentWindow.mediaItem.mediaId,
+                isDisplayed = true,
+                onDismiss = { },
+                height = thumbnailHeight,
+                mediaMetadataProvider = currentWindow.mediaItem::mediaMetadata,
+                durationProvider = player::getDuration,
+                ensureSongInserted = { Database.insert(currentWindow.mediaItem) },
+                onMenuLaunched = onDismiss
+            )
+        }
     }
 }
