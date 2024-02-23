47897862fef8e6b81a1a72d811ccb51acfadaea7
25huizengek1 <50515369+25huizengek1@users.noreply.github.com>
2024-02-23 18:57:44 +0100

    Improve MusicBars implementation
    - Move constants out of Composable and use ImmutableList (Compose compiler optimizations)
    - Combine all canvases into a single canvas
    - Use fastForEach in Canvas (called almost every frame)
    - Clean up code
    - Remove useless allocations
    - Avoid relaunching the animation loop (a for loop inside a while loop is unneccesary)

--- app/src/main/kotlin/it/vfsfitvnm/vimusic/ui/components/MusicBars.kt
+++ app/src/main/kotlin/it/vfsfitvnm/vimusic/ui/components/MusicBars.kt
@@ -2,148 +2,80 @@ package it.vfsfitvnm.vimusic.ui.components
 
 import androidx.compose.animation.core.Animatable
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.fastForEachIndexed
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
+import okhttp3.internal.immutableListOf
+
+// @formatter:off
+@Suppress("MaximumLineLength")
+private val steps = immutableListOf(
+    arrayOf(0.8f, 0.1f, 0.9f, 0.9f, 0.7f, 0.9f, 0.8f, 0.1f, 0.3f, 0.8f, 0.6f, 0.0f, 0.3f, 0.4f, 0.9f, 0.7f, 0.9f, 0.6f, 0.9f, 0.1f, 0.3f, 0.0f, 0.5f, 0.4f, 0.7f, 0.9f),
+    arrayOf(0.8f, 0.5f, 0.0f, 0.5f, 0.7f, 0.9f, 0.8f, 0.7f, 0.5f, 0.9f, 0.4f, 0.5f, 0.7f, 0.3f, 0.1f, 0.0f, 0.7f, 0.9f, 0.5f, 0.7f, 0.4f, 0.0f, 0.4f, 0.3f, 0.6f, 0.9f),
+    arrayOf(0.4f, 0.5f, 0.0f, 0.4f, 0.5f, 0.0f, 0.4f, 0.5f, 0.0f, 0.5f, 0.4f, 0.3f, 0.8f, 0.7f, 0.9f, 0.5f, 0.6f, 0.4f, 0.3f, 0.9f, 0.6f, 0.7f, 0.9f, 0.6f, 0.7f, 0.3f)
+)
+// @formatter:on
 
 @Composable
 fun MusicBars(
     color: Color,
     modifier: Modifier = Modifier,
     barWidth: Dp = 4.dp,
-    cornerRadius: Dp = 16.dp
+    cornerRadius: Dp = 16.dp,
+    space: Dp = 4.dp
 ) {
-    val animatablesWithSteps = remember {
-        listOf(
-            Animatable(0f) to listOf(
-                0.2f,
-                0.8f,
-                0.1f,
-                0.1f,
-                0.3f,
-                0.1f,
-                0.2f,
-                0.8f,
-                0.7f,
-                0.2f,
-                0.4f,
-                0.9f,
-                0.7f,
-                0.6f,
-                0.1f,
-                0.3f,
-                0.1f,
-                0.4f,
-                0.1f,
-                0.8f,
-                0.7f,
-                0.9f,
-                0.5f,
-                0.6f,
-                0.3f,
-                0.1f
-            ),
-            Animatable(0f) to listOf(
-                0.2f,
-                0.5f,
-                1.0f,
-                0.5f,
-                0.3f,
-                0.1f,
-                0.2f,
-                0.3f,
-                0.5f,
-                0.1f,
-                0.6f,
-                0.5f,
-                0.3f,
-                0.7f,
-                0.8f,
-                0.9f,
-                0.3f,
-                0.1f,
-                0.5f,
-                0.3f,
-                0.6f,
-                1.0f,
-                0.6f,
-                0.7f,
-                0.4f,
-                0.1f
-            ),
-            Animatable(0f) to listOf(
-                0.6f,
-                0.5f,
-                1.0f,
-                0.6f,
-                0.5f,
-                1.0f,
-                0.6f,
-                0.5f,
-                1.0f,
-                0.5f,
-                0.6f,
-                0.7f,
-                0.2f,
-                0.3f,
-                0.1f,
-                0.5f,
-                0.4f,
-                0.6f,
-                0.7f,
-                0.1f,
-                0.4f,
-                0.3f,
-                0.1f,
-                0.4f,
-                0.3f,
-                0.7f
-            )
-        )
-    }
+    val animatables = remember { List(steps.size) { Animatable(0f) } }
 
     LaunchedEffect(Unit) {
-        animatablesWithSteps.forEach { (animatable, steps) ->
+        animatables.fastForEachIndexed { i, animatable ->
             launch {
-                while (true) {
-                    steps.forEach { step ->
-                        animatable.animateTo(step)
-                    }
+                var step = 0
+                val steps = steps[i]
+                while (isActive) {
+                    animatable.animateTo(steps[step])
+                    step = (step + 1) % steps.size
                 }
             }
         }
     }
 
-    Row(
-        horizontalArrangement = Arrangement.spacedBy(4.dp),
-        verticalAlignment = Alignment.Bottom,
+    Canvas(
         modifier = modifier
+            .fillMaxHeight()
+            .width(barWidth * animatables.size + space * animatables.lastIndex)
     ) {
-        animatablesWithSteps.forEach { (animatable) ->
-            Canvas(
-                modifier = Modifier
-                    .fillMaxHeight()
-                    .width(barWidth)
-            ) {
-                drawRoundRect(
-                    color = color,
-                    topLeft = Offset(x = 0f, y = size.height * (1 - animatable.value)),
-                    size = size.copy(height = animatable.value * size.height),
-                    cornerRadius = CornerRadius(cornerRadius.toPx())
-                )
-            }
+        val radius = CornerRadius(cornerRadius.toPx())
+        val barWidthPx = barWidth.toPx()
+        val barHeightPx = size.height
+        val stride = barWidthPx + space.toPx()
+
+        animatables.fastForEachIndexed { i, animatable ->
+            val value = animatable.value
+
+            drawRoundRect(
+                color = color,
+                topLeft = Offset(
+                    x = i * stride,
+                    y = barHeightPx * value
+                ),
+                size = Size(
+                    width = barWidthPx,
+                    height = barHeightPx * (1 - value)
+                ),
+                cornerRadius = radius
+            )
         }
     }
 }
