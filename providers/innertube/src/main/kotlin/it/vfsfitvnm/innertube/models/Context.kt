8605b349917277e6fdd6cc672014ef0c4dbf8eb8
25huizengek1 <50515369+25huizengek1@users.noreply.github.com>
2024-02-27 02:03:11 +0100

    Fallback to English when language is not supported by YouTube Music (#90)

--- providers/innertube/src/main/kotlin/it/vfsfitvnm/innertube/models/Context.kt
+++ providers/innertube/src/main/kotlin/it/vfsfitvnm/innertube/models/Context.kt
@@ -44,14 +44,22 @@ data class Context(
     companion object {
         const val DEFAULT_VISITOR_DATA = "CgtsZG1ySnZiQWtSbyiMjuGSBg%3D%3D"
 
-        val DefaultWeb get() = DefaultWebNoLang.let {
-            it.copy(
-                client = it.client.copy(
-                    hl = Locale.getDefault().toLanguageTag(),
-                    gl = Locale.getDefault().country
+        val DefaultWeb
+            get() = DefaultWebNoLang.let { context ->
+                val locale = Locale.getDefault()
+
+                context.copy(
+                    client = context.client.copy(
+                        hl = locale
+                            .toLanguageTag()
+                            .replace("-Hant", "")
+                            .takeIf { it in validLanguageCodes } ?: "en",
+                        gl = locale
+                            .country
+                            .takeIf { it in validCountryCodes } ?: "US"
+                    )
                 )
-            )
-        }
+            }
 
         val DefaultWebNoLang = Context(
             client = Client(
@@ -83,3 +91,13 @@ data class Context(
         )
     }
 }
+
+// @formatter:off
+@Suppress("MaximumLineLength")
+val validLanguageCodes =
+    listOf("af", "az", "id", "ms", "ca", "cs", "da", "de", "et", "en-GB", "en", "es", "es-419", "eu", "fil", "fr", "fr-CA", "gl", "hr", "zu", "is", "it", "sw", "lt", "hu", "nl", "no", "or", "uz", "pl", "pt-PT", "pt", "ro", "sq", "sk", "sl", "fi", "sv", "bo", "vi", "tr", "bg", "ky", "kk", "mk", "mn", "ru", "sr", "uk", "el", "hy", "iw", "ur", "ar", "fa", "ne", "mr", "hi", "bn", "pa", "gu", "ta", "te", "kn", "ml", "si", "th", "lo", "my", "ka", "am", "km", "zh-CN", "zh-TW", "zh-HK", "ja", "ko")
+
+@Suppress("MaximumLineLength")
+val validCountryCodes =
+    listOf("DZ", "AR", "AU", "AT", "AZ", "BH", "BD", "BY", "BE", "BO", "BA", "BR", "BG", "KH", "CA", "CL", "HK", "CO", "CR", "HR", "CY", "CZ", "DK", "DO", "EC", "EG", "SV", "EE", "FI", "FR", "GE", "DE", "GH", "GR", "GT", "HN", "HU", "IS", "IN", "ID", "IQ", "IE", "IL", "IT", "JM", "JP", "JO", "KZ", "KE", "KR", "KW", "LA", "LV", "LB", "LY", "LI", "LT", "LU", "MK", "MY", "MT", "MX", "ME", "MA", "NP", "NL", "NZ", "NI", "NG", "NO", "OM", "PK", "PA", "PG", "PY", "PE", "PH", "PL", "PT", "PR", "QA", "RO", "RU", "SA", "SN", "RS", "SG", "SK", "SI", "ZA", "ES", "LK", "SE", "CH", "TW", "TZ", "TH", "TN", "TR", "UG", "UA", "AE", "GB", "US", "UY", "VE", "VN", "YE", "ZW")
+// @formatter:on
